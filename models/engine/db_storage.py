7d5a5e80c36502993eadda4f137d3440b1a5736e
Jesse <ibbraidejesse@gmail.com>
2022-12-10 08:47:21 +0100

    added save and new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,8 +5,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.scoping import scoped_session
 from models.base_model import Base
+from models.user import User
 
 
 class DBStorage:
     __engine = None
     __session = None
+
+    def __init__(self):
+        user, password = getenv('user'), getenv('password')
+        host, db = getenv('host'), getenv('db')
+
+        url = f'mysql+mysqldb://{user}:{password}@{host}/{db}'
+
+        self.__engine = create_engine(url, pool_pre_ping=True)
+
+
+    def reload(self):
+        ''' reload engine '''
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+        self.__session = Session()
+
+    def new(self, obj):
+        '''adds a new record (object) to session'''
+        self.__session.add(obj)
+
+    def save(self):
+        ''' saves a new record (object) to table '''
+        self.__session.commit()
