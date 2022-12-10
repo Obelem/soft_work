116af8cfafa5334096c04b7352909587e1ac554d
Franklin Obasi <franklinobasy@gmail.com>
2022-12-10 01:31:27 +0100

    modfied user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, DateTime
 '''
 user.py
 '''
@@ -14,6 +12,7 @@ class User(BaseModel, Base):
     '''defines user class'''
     __tablename__ = 'users'
 
+    username = Column(String(60), nullable=False, unique=True)
     first_name = Column(String(60), nullable=False)
     middle_name = Column(String(60), nullable=True)
     last_name = Column(String(60), nullable=False)
