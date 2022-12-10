c4c6a88ffeb03387cdd15df0d509eb52f82032f8
Jesse <ibbraidejesse@gmail.com>
2022-12-10 01:17:56 +0100

    defined the user class

--- models/user.py
+++ models/user.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, DateTime
 
 
 class User(BaseModel, Base):
     '''defines user class'''
+    __tablename__ = 'users'
 
+    first_name = Column(String(60), nullable=False)
+    middle_name = Column(String(60), nullable=True)
+    last_name = Column(String(60), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(60), nullable=False)
