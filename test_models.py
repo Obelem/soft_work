7ca5e531ab7aa369fe9126681dfb03185178459a
Jesse <ibbraidejesse@gmail.com>
2022-12-10 11:33:51 +0100

    test marking_scheme

--- test_models.py
+++ test_models.py
@@ -6,9 +6,10 @@
 from models.base_model import BaseModel
 from models.user import User
 from models.assessments import Assessment
+from models.marking_scheme import MarkingScheme
 
-base = BaseModel()
-print(base)
+# base = BaseModel()
+# print(base)
 
 # user = User(
 #             username='JBez',
@@ -21,11 +22,28 @@ print(base)
 # print(user)
 
 options = ['Europe', 'Asia', 'Africa']
-assessment = Assessment(
-    question_test='What continent is Nigeria in?',
+
+assessment = Assessment(name='Critical Thinking')
+assessment.save()
+
+marking_scheme = MarkingScheme(
+    assessment_id=assessment.id,
+    question='which continent is Nigeria found?',
     options=options,
     answer='Africa'
 )
-assessment.save()
-print(assessment.options)
+
+marking_scheme.save()
+
+print(assessment.id)
+print(marking_scheme.assessment_id)
+print(marking_scheme.options)
+print(marking_scheme.options[0])
+
+print('making changes')
+
+marking_scheme.options[0] = 'Australia'
+marking_scheme.save()
+
+print(marking_scheme.options[0])
 
