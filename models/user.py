d52715cc873ecba049657b1364942e4f462681ff
Franklin Obasi <franklinobasy@gmail.com>
2022-12-10 01:26:08 +0100

    modfied user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel, Base
+'''
+user.py
+'''
+
+from .base_model import BaseModel, Base
+
+from sqlalchemy import Column, Integer, String
 
 
 class User(BaseModel, Base):
     '''defines user class'''
-
+    email = Column(String(50), nullable=False, unique=True)
+    username = Column(String(50), nullable=False, primary_key=True, unique=True)
+    password = Column(String(50), nullable=False)
