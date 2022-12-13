777d9979c6eb6de1c2fcb523429e3f79cfbe65bd
Franklin Obasi <franklinobasy@gmail.com>
2022-12-13 15:55:59 +0100

    login and signup implemented

--- web/landing/routes.py
+++ web/landing/routes.py
@@ -2,7 +2,8 @@ from flask import abort, jsonify, render_template, request, url_for
 from web.landing import landing_views
 
 
-@landing_views.route("/", methods=['GET'], strict_slashes=False)
+@landing_views.route("/", methods=['GET'])
+@landing_views.route("/welcome/", methods=['GET'])
 def landing_page():
     return render_template("landing/index.html")
 
