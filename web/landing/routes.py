f0656bdb4a6be618a3c418756b96ad9a2f4711ff
Franklin Obasi <franklinobasy@gmail.com>
2022-12-13 23:34:30 +0100

    modified landing page

--- web/landing/routes.py
+++ web/landing/routes.py
@@ -1,9 +1,10 @@
 from flask import abort, jsonify, render_template, request, url_for
+from flask_login import current_user
 from web.landing import landing_views
 
 
 @landing_views.route("/", methods=['GET'])
 @landing_views.route("/welcome/", methods=['GET'])
 def landing_page():
-    return render_template("landing/index.html")
+    return render_template("landing/index.html", current_user=current_user)
 
