dc2bbe02fc442d03e3a2c73bb5a92bc5d99897e7
Jesse <ibbraidejesse@gmail.com>
2023-01-09 08:47:42 +0100

    refactored delete user

--- web/api/v1/views/delete_user.py
+++ web/api/v1/views/delete_user.py
@@ -2,7 +2,7 @@
 '''views for assessment status of current user'''
 from web.api.v1.views import app_views
 from flask import jsonify, abort, request, redirect, url_for, render_template
-from flask_login import login_required, current_user
+from flask_login import login_required, current_user, logout_user
 from models import storage
 
 
@@ -10,7 +10,6 @@ from models import storage
                 strict_slashes=False)
 @login_required
 def delete_user():
-    logout_user()
     storage.delete(current_user)
     storage.save()
 
